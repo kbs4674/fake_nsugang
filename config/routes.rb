d4bfc0e451fd48d7eddec19587655435490a18a4
EC2 Default User <ec2-user@ip-172-31-19-90.ap-northeast-1.compute.internal>
2019-09-24 17:16:29 +0000

    Devise 수정

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
     require 'sidekiq/web'
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    devise_scope :user do
+        root to: "devise/sessions#new"
+    end
     
+    devise_for :users, controllers: { sessions: 'users/sessions' }
     mount Sidekiq::Web => '/sidekiq'
 end
